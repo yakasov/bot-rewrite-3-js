e731782aadf70674dda8d5e160acdd56d321e6db
yakasov <yakasovian@gmail.com>
2023-03-26 18:59:52 +0100

    feat: add youtube sing

---
+++ commands/sing.js
@@ -0,0 +1,18 @@
+const { createAudioPlayer, joinVoiceChannel, createAudioResource } = require('@discordjs/voice')
+const ytdl = require('ytdl-core-discord');
+
+exports.run = async(client, msg, args) => {
+    if (!args.length) {
+        return;
+    }
+
+    const player = createAudioPlayer();
+    joinVoiceChannel({
+        channelId: msg.member.voice.channelId,
+        guildId: msg.guild.id,
+        adapterCreator: msg.guild.voiceAdapterCreator
+    }).subscribe(player);
+
+    const res = createAudioResource(await ytdl(args[0]))
+    player.play(res)
+}
