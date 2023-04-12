8c9511aa319a8dbe5f55772776e752151625bb1f
yakasov <yakasovian@gmail.com>
2023-04-12 12:57:16 +0100

    fix: actually do the previous fix

--- tasks/birthdays.js
+++ tasks/birthdays.js
@@ -28,7 +28,7 @@ exports.run = async (client, date, force = false) => {
       if (
         birthdays[m.id] &&
         birthdays[m.id].date === today &&
-        !roleMembers[m]
+        !roleMembers.includes(m.id)
       ) {
         m.roles.add(bdayRoleId);
         bdayChannel.send(
