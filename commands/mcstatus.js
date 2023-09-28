a4cc538b6b56998aec3ae02ed2ae18cb337f4ba3
yakasov <yakasovian@gmail.com>
2023-09-28 19:15:24 +0100

    convert mcstatus.js to module.exports (instead of exports.run)

--- commands/mcstatus.js
+++ commands/mcstatus.js
@@ -5,23 +5,27 @@ const {
   minecraftServerOwnerId,
 } = require("./../resources/config.json");
 
-exports.run = async (client, msg, args) => {
-  if (!(minecraftServerIp.length && minecraftServerPort)) {
-    return;
-  }
+module.exports = {
+  aliases: [],
+  description: "Get information about the current Minecraft server",
+  run: async (client, msg, args) => {
+    if (!(minecraftServerIp.length && minecraftServerPort)) {
+      return;
+    }
 
-  status(minecraftServerIp, minecraftServerPort)
-    .then((res) => {
-      res.favicon = null; // favicon is a base64 encoded image, remove it
-      msg.reply("```\n" + JSON.stringify(res, null, 4) + "\n```");
-    })
-    .catch((e) => {
-      var str = e.message;
+    status(minecraftServerIp, minecraftServerPort)
+      .then((res) => {
+        res.favicon = null; // favicon is a base64 encoded image, remove it
+        msg.reply("```\n" + JSON.stringify(res, null, 4) + "\n```");
+      })
+      .catch((e) => {
+        var str = e.message;
 
-      if (e.errno && e.errno === -4078) {
-        str += `\n<@${minecraftServerOwnerId}>`;
-      }
+        if (e.errno && e.errno === -4078) {
+          str += `\n<@${minecraftServerOwnerId}>`;
+        }
 
-      msg.reply(str);
-    });
+        msg.reply(str);
+      });
+  },
 };
