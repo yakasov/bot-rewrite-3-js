c3b311c4eff0f4f8b849bc127297d02849c0148f
yakasov <yakasovian@gmail.com>
2024-02-01 19:22:30 +0000

    Potential TTS improvement

--- commands/tts.js
+++ commands/tts.js
@@ -3,15 +3,15 @@ const {
   joinVoiceChannel,
   createAudioResource,
 } = require("@discordjs/voice");
-const gTTS = require("gtts");
+const fs = require("node:fs");
 
 module.exports = {
   aliases: [],
   description: "Generate a TTS output from a given input",
   run: async (client, msg, args) => {
-    if (!args.length) {
-      return;
-    }
+    // if (!args.length) {
+    //   return;
+    // }
 
     const player = createAudioPlayer();
     joinVoiceChannel({
@@ -20,8 +20,30 @@ module.exports = {
       adapterCreator: msg.guild.voiceAdapterCreator,
     }).subscribe(player);
 
-    var gtts = new gTTS(`${args.join(" ")}`, "en");
-    const res = createAudioResource(gtts.stream());
+    const response = await fetch(
+      "https://tiktok-tts.weilnet.workers.dev/api/generation",
+      {
+        method: "post",
+        body: JSON.stringify({
+          text: "The fungus among us",
+          voice: "en_us_001",
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    const data = await response.json();
+
+    const res = createAudioResource(
+      await module.exports.audioToBase64(`data:audio/mpeg;base64,${data.data}`)
+    );
     player.play(res);
+  },
+  audioToBase64: async (audioFile) => {
+    return new Promise((resolve, reject) => {
+      let reader = new fs();
+      reader.onerror = reject;
+      reader.onload = (e) => resolve(e.target.result);
+      reader.readAsDataURL(audioFile);
+    });
   },
 };
