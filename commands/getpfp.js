ae5c30c276a8111b7871fee4947991589fdc9f78
yakasov <yakasovian@gmail.com>
2024-02-19 14:24:26 +0000

    Fix *getpfp and *sing crashing with bad args

--- commands/getpfp.js
+++ commands/getpfp.js
@@ -5,19 +5,23 @@ module.exports = {
   description:
     "Get profile picture of mentioned user or from user ID. If no mention or ID, use author",
   run: async (client, msg, args) => {
-    var avatar;
-    if (args.length) {
-      const member =
-        msg.mentions.members.first() ||
-        (await msg.guild.members.fetch(args[0]));
-      avatar = member.displayAvatarURL({ size: 1024, dynamic: true });
-    } else {
-      avatar = msg.author.displayAvatarURL({ size: 1024, dynamic: true });
-    }
+    try {
+      var avatar;
+      if (args.length) {
+        const member =
+          msg.mentions.members.first() ||
+          (await msg.guild.members.fetch(args[0]));
+        avatar = member.displayAvatarURL({ size: 1024, dynamic: true });
+      } else {
+        avatar = msg.author.displayAvatarURL({ size: 1024, dynamic: true });
+      }
 
-    const embed = new EmbedBuilder()
-      .setImage(avatar)
-      .setAuthor({ name: msg.author.username });
-    msg.channel.send({ embeds: [embed] });
+      const embed = new EmbedBuilder()
+        .setImage(avatar)
+        .setAuthor({ name: msg.author.username });
+      msg.channel.send({ embeds: [embed] });
+    } catch (e) {
+      msg.reply(e.message);
+    }
   },
 };
