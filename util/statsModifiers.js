e55c07872781bc9dce8a75dff293e9028c0d9e3b
yakasov <yakasovian@gmail.com>
2024-06-16 16:47:32 +0100

    Add /statsModifiers placeholder

---
+++ util/statsModifiers.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/*
+ * The plan here is to override functions defined in stats.js.
+ *
+ * This allows me to apply 'seasonal modifiers' without messing with
+ * the core functions - the modifiers will only apply to mainGuild.
+ * 
+ * Essentially, if guildId === mainGuildId, use 'statsModifiers.func'
+ * instead of 'stats.func'.
+ */
