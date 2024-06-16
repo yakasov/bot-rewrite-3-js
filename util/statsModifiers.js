a61cecfe008455145f094867eaa2ba18fe0c5392
yakasov <yakasovian@gmail.com>
2024-06-16 20:32:37 +0100

    Add some season 2 modifiers

--- util/statsModifiers.js
+++ util/statsModifiers.js
@@ -1,21 +1,61 @@
 "use strict";
 
+const { statsConfig } = require("../resources/config.json");
+
 /*
  * The plan here is to override functions defined in stats.js.
  *
  * This allows me to apply 'seasonal modifiers' without messing with
  * the core functions - the modifiers will only apply to mainGuild.
- * 
+ *
  * Essentially, if guildId === mainGuildId, use 'statsModifiers.func'
  * instead of 'stats.func'.
  */
 
 module.exports = {
-  updateNerdCoolScores: (guildId, userId) => {},
+  updateNerdCoolScores: (guildId, userId) => {
+    const nerdPower =
+      globalThis.stats[guildId][userId].prestige > 0 ? 4 : 2.5;
+    globalThis.stats[guildId][userId].nerdScore =
+      Object.values(globalThis.stats[guildId][userId].nerdEmojis)
+        .reduce(
+          (sum, a) => sum + Math.max(nerdPower ** a + 1, 0) - 1,
+          0
+        ) - globalThis.stats[guildId][userId].nerdHandicap;
 
-  updateScoreValue: (guildId, userId) => {},
+    globalThis.stats[guildId][userId].coolScore =
+      Object.values(globalThis.stats[guildId][userId].coolEmojis)
+        .reduce(
+          (sum, a) => sum + Math.max(4 ** a + 1, 0) - 1,
+          0
+        ) - globalThis.stats[guildId][userId].coolHandicap;
+  },
 
-  updateScores: () => {},
+  updateScoreValue: (guildId, userId) => {
+    const score = Math.floor(
+      (globalThis.stats[guildId][userId].voiceTime *
+        statsConfig.voiceChatSRGain +
+        globalThis.stats[guildId][userId].messages *
+          statsConfig.messageSRGain) *
+        Math.max(
+          1 +
+            globalThis.stats[guildId][userId].reputation *
+              statsConfig.reputationGain,
+          0.001
+        ) *
+        1.5 ** globalThis.stats[guildId][userId].prestige +
+        globalThis.stats[guildId][userId].luckHandicap +
+        globalThis.stats[guildId][userId].coolScore -
+        globalThis.stats[guildId][userId].nerdScore
+    );
 
-  updateStatsOnPrestige: (userStats) => {}
+    if (
+      score > statsConfig.prestigeRequirement &&
+      globalThis.stats[guildId][userId].prestige < statsConfig.prestigeMaximum
+    ) {
+      globalThis.stats[guildId][userId].score = statsConfig.prestigeRequirement;
+    } else {
+      globalThis.stats[guildId][userId].score = score;
+    }
+  },
 };
