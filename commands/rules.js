d82e005b3a7ea924233f1d1552d0d682cdc23c1f
James McDougall <james.mcdougall2@theaccessgroup.com>
2025-02-28 14:54:05 +0000

    Add /rules

---
+++ commands/rules.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  "data": new SlashCommandBuilder()
+    .setName("rules")
+    .setDescription("The rules."),
+  async execute(interaction) {
+    const rules = await fetch("https://jmcd.uk/bot/getRules", {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((r) => r.json())
+        .then((j) => j.data);
+
+    let output = "";
+    Object.entries(rules).forEach(([k, v]) => {
+        output += `Rule ${k}: ${v}\n`
+    })
+    interaction.reply(output);
+  }
+};
