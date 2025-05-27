1ca1dcaa3ef93759e671c38ebafe422118238746
yakasov <yakasovian@gmail.com>
2025-05-27 21:01:49 +0100

    Emote check

--- tasks/fortnite.js
+++ tasks/fortnite.js
@@ -6,6 +6,8 @@ const {
 } = require("../resources/config.json");
 
 let currentSongs = [];
+let isGriddyInShop = false;
+let isHomerInShop = false;
 
 async function getFortniteShop() {
   try {
@@ -25,9 +27,25 @@ exports.run = async (client) => {
   const data = await getFortniteShop();
   const songs = data.entries.filter((e) => e.layout.name === "Jam Tracks")
     .map((e) => `${e.tracks[0].title  } - ${  e.tracks[0].artist}`);
+  const emotes = data.entries.filter((e) => e.brItems && e.brItems[0].type.value === "emote")
+    .map((e) => e.brItems[0].name);
 
   const guild = await client.guilds.fetch(mainGuildId);
   const fortniteChannel = await guild.channels.fetch(fortniteChannelId);
+
+  if (emotes.includes("Get Griddy") && !isGriddyInShop) {
+    isGriddyInShop = true;
+    fortniteChannel.send("Get Griddy is now in the Fortnite shop!");
+  } else {
+    isGriddyInShop = false;
+  }
+
+  if (emotes.includes("Bring It Around") && !isHomerInShop) {
+    isHomerInShop = true;
+    fortniteChannel.send("The Homer Simpson dance is now in the Fortnite shop!");
+  } else {
+    isHomerInShop = false;
+  }
 
   if (currentSongs.length === 0) {
     currentSongs = songs;
