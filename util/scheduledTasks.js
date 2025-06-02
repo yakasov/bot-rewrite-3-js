e13ae34c1de65ea55fde9b6290931eaeb5773d4e
James McDougall <james.mcdougall2@theaccessgroup.com>
2025-06-02 10:56:03 +0100

    Update scheduledTasks to properly use async

--- util/scheduledTasks.js
+++ util/scheduledTasks.js
@@ -6,12 +6,12 @@ function getTime(seconds = 0, minutes = 0, hours = 0) {
   return 1000 * seconds + 1000 * 60 * minutes + 1000 * 60 * 60 * hours;
 }
 
-function checkBirthdays(force = false) {
-  require("../tasks/birthdays.js").run(globalThis.client, force);
+async function checkBirthdays(force = false) {
+  await require("../tasks/birthdays.js").run(globalThis.client, force);
 }
 
-function checkFortniteShop() {
-  require("../tasks/fortnite.js").run(globalThis.client);
+async function checkFortniteShop() {
+  await require("../tasks/fortnite.js").run(globalThis.client);
 }
 
 async function checkMinecraftServer() {
@@ -22,7 +22,7 @@ async function checkMinecraftServer() {
 }
 
 function getNewSplash() {
-  globalThis.splash = npFile.run([globalThis.client]);
+  return npFile.run([globalThis.client]);
 }
 
 module.exports = {
